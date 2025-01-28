fec41c828335e2b329340304b8ed1d1fae67eff9
NovaDevSas <novadevsas@gmail.com>
2025-01-28 15:57:39 -0500

    Remove reporting and scheduled reporting functionality, including controllers, routes, models, and utility functions

--- Backend server/index.js
+++ Backend server/index.js
@@ -9,11 +9,7 @@ const projectRoutes = require('./routes/projectRoutes');
 const financeItemRoutes = require('./routes/financeItemRoutes');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-const reportRoutes = require('./routes/reportRoutes');
-const scheduledReportRoutes = require('./routes/scheduledReportRoutes'); // Importar las rutas de reportes programados
 const { swaggerUi, specs } = require('./config/swagger');
-const ScheduledReport = require('./models/ScheduledReport');
-const { scheduleReport } = require('./controllers/scheduledReportController'); // Importar la función para programar reportes
 require('dotenv').config();
 
 const app = express();
@@ -38,25 +34,9 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/finance-items', financeItemRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
-app.use('/api/reports', reportRoutes);
-app.use('/api/scheduled-reports', scheduledReportRoutes); // Usar las rutas de reportes programados
 
 // Documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-// Programar los reportes al iniciar el servidor
-const initializeScheduledReports = async () => {
-  console.log('Initializing scheduled reports...');
-  try {
-    const scheduledReports = await ScheduledReport.find();
-    console.log('Scheduled Reports:', scheduledReports);
-    scheduledReports.forEach(scheduleReport);
-  } catch (error) {
-    console.error('Error initializing scheduled reports:', error);
-  }
-};
-
-initializeScheduledReports();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
